f234f80da0c221945bb0a0a0a90b5c9120e15e45
jjmuchai <jjmuchai151@gmail.com>
2025-03-25 15:13:11 +0300

    My updates to the project

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression for divide
+const divide = function(a = 2000, b = 100) {
+    return a / b;
+};
+
+// Arrow function for square
+const square = x => x * x;
+
+// Arrow function for add
+const add = (a, b) => a + b;
+
+// Example usages (optional, just for testing)
+console.log(divide()); // 20
+console.log(square(2)); // 4
+console.log(add(3, 4)); // 7
